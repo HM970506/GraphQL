30efe3bc973201b758ea56175ec016096a7f8ab7
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-09-09 23:51:24 +0900

    resolve를 써보자

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 import {ApolloServer, gql} from "apollo-server";
+const tweets = [
+    {
+      id: "1",
+      text: "first one!",
+    },
+    {
+      id: "2",
+      text: "second one",
+    },
+  ];
 
+let users = [
+    {
+      id: "1",
+      firstName: "nico",
+      lastName: "las",
+    },
+    {
+      id: "2",
+      firstName: "Elon",
+      lastName: "Mask",
+    },
+  ];
+  
+  
 
 const typeDefs = gql`
     type User{
@@ -12,15 +36,30 @@ const typeDefs = gql`
         author: User
     }
     type Query{
-        allTweets: [Tweet]
-        tweet(id: ID): Tweet
+        allTweets: [Tweet!]!
+        tweet(id: ID): Tweet!
+        ping: String!
     }
 	type Mutation{
     	postTweet(text: String, userId: ID): Tweet
     }
 `;
 
-const server = new ApolloServer({typeDefs});
+const resolvers ={
+    Query:{
+        allTweets(){
+            return tweets;
+        },
+        tweet(root, {id}){
+            return tweets.find((tweet)=>tweet.id===id);
+        },
+        ping(){
+            return "pong";
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({url})=>{console.log(`${url}서버를 열었습니다~`)});
 
